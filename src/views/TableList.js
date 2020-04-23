1de990e9dc2bef26065bafe82dc832b277000262
contactbeckfriends <contactbeckfriends@gmail.com>
2020-04-23 11:54:56 +0530

    23rd

--- src/views/TableList.js
+++ src/views/TableList.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React , { forwardRef } from "react";
 import firebaseinit from '../credentials';
 import IconButton from '@material-ui/core/IconButton';
 import Skeleton from '@material-ui/lab/Skeleton';
+import MaterialTable from "material-table";
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 import {
   Card,
   CardHeader,
@@ -13,10 +29,38 @@ import {
   Col
 } from "reactstrap";
 import Button from '@material-ui/core/Button';
-
+import {
+  useTable,
+  usePagination,
+  useResizeColumns,
+  useFlexLayout,
+  useSortBy,
+  useRowSelect,
+} from 'react-table'
+import styled from 'styled-components';
 const database = firebaseinit.database();
 
 const merchantorders = [];
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
 
 class Tables extends React.Component {
 
@@ -51,6 +95,23 @@ class Tables extends React.Component {
 
   render() {
     const merchantorderslist = this.state.merchantorderslist;
+
+  const columns = [
+    {
+      Header: 'PNR',
+      accessor: 'uid'
+    }, {
+      Header: 'Number',
+      accessor: 'number'
+    }, {
+      Header: 'From',
+      accessor: 'from'
+    }, {
+      Header: 'To',
+      accessor: 'to'
+    }
+  ];
+
     return (
       <>
         <div className="content">
@@ -62,47 +123,21 @@ class Tables extends React.Component {
                   <p className="category">All travelers across various routes</p>
                 </CardHeader>
                 <CardBody>
-                  {this.state.added && <Table className="tablesorter" responsive>
-                    <thead className="text-primary">
-                      <tr style={{ textAlign: 'center' }}>
-                        <th>PNR</th>
-                        <th>Flight No</th>
-                        <th>From</th>
-                        <th>To</th>
-                        <th>Number</th>
-                        <th>Weight</th>
-                        <th>Discount</th>
-                        <th>Class</th>
-                        <th>Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody style={{ textAlign: 'center' }}>
-                      {merchantorderslist.length ? 
-                        merchantorderslist.map(merchantorder => (
-                          <tr key={merchantorder.uid}>
-                            <td>{merchantorder.uid}</td>
-                            <td>BA 96</td>
-                            <td>{merchantorder.from}</td>
-                            <td>{merchantorder.to}</td>
-                            <td>{merchantorder.number}</td>
-                            <td>{merchantorder.weight}</td>
-                            <td>{merchantorder.discount}</td>
-                            <td><a href="#">{merchantorder.classoption}</a></td>
-                            <td><IconButton>
-   <AccessAlarmIcon style={{ color: 'green' }}/>
-</IconButton></td>
-                          </tr>
-                        ))
-                        : 
-                        (<tr>
-                          <td>-</td>
-                          <td>-</td>
-                          <td>-</td>
-                          <td>-</td>
-                        </tr>)
-                      }
-                    </tbody>
-                  </Table>}
+                  
+                  <MaterialTable
+                  icons={tableIcons}
+          columns={[
+            { title: "PNR", field: "uid" },
+            { title: "From", field: "from" },
+            { title: "To", field: "to" },
+            { title: "Number", field: "number" },
+            { title: "Weight", field: "weight" },
+            { title: "Discount", field: "discount" },
+            { title: "Class", field: "classoption" }
+          ]}
+          data={merchantorderslist}
+          title="Demo Title"
+        />
                 </CardBody>
               </Card>
             </Col>
