4be6b2e79671d71eb07e93211815afa0c4031222
contactbeckfriends <contactbeckfriends@gmail.com>
2020-05-29 21:58:24 +0530

    Updates

---
+++ precache-manifest.6c17ac469cc2d05ce93b2196df3bef8d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c748d1da9acc220995f3467d1c7524bc",
+    "url": "/beckbag/index.html"
+  },
+  {
+    "revision": "ebaa725739ab993ecff1",
+    "url": "/beckbag/static/css/main.5786c49c.chunk.css"
+  },
+  {
+    "revision": "8eb1789f3ad06e0ac55c",
+    "url": "/beckbag/static/js/2.dd21fb6f.chunk.js"
+  },
+  {
+    "revision": "8a8ebf431c20560de1b9521f47fd6c0a",
+    "url": "/beckbag/static/js/2.dd21fb6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebaa725739ab993ecff1",
+    "url": "/beckbag/static/js/main.398a9842.chunk.js"
+  },
+  {
+    "revision": "dc192b57bfed2afd6be1ea5558f17255",
+    "url": "/beckbag/static/js/main.398a9842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2aeb872de77c1c8e078",
+    "url": "/beckbag/static/js/runtime-main.5419615e.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/beckbag/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/beckbag/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/beckbag/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/beckbag/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/beckbag/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/beckbag/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/beckbag/static/media/react-logo.eb6be414.png"
+  }
+]);
